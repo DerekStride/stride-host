28d986b7f9fcd31091ee41cdef5f3736e8e0de99
derekstride <derek@stride.host>
2022-08-18 16:42:59 -0400

    Implement status subcommand

--- functions/mc-scale-event/app.rb
+++ functions/mc-scale-event/app.rb
@@ -13,7 +13,7 @@ def mc_scale(apps_client, replicas:)
   apps_client.patch_stateful_set("mc-lutova", { spec: { replicas: replicas } }, "default")
 rescue Kubeclient::HttpError => e
   raise FunctionException, <<~ERROR
-    Scaling the statefulset failed due to Kubernetes Error: "#{e.class}"
+    Scaling the StatefulSet failed due to Kubernetes Error: "#{e.class}"
 
     #{e.message}
   ERROR
@@ -29,24 +29,32 @@ def send_follow_up(message, app_id:, interaction_token:)
   )
 end
 
-def build_kube_client
+def kube_client_options
   config = Kubeclient::Config.read(ENV["KUBE_CONFIG"])
   authorizer = Google::Auth::ServiceAccountCredentials.from_env(scope: SCOPE)
   token = authorizer.fetch_access_token!
   context = config.context
 
-  Kubeclient::Client.new(
-    "#{context.api_endpoint}/apis/apps",
-    context.api_version,
+  {
+    api_endpoint: context.api_endpoint,
+    api_version: context.api_version,
     ssl_options: context.ssl_options,
-    auth_options: {
-      bearer_token: token["access_token"],
-    },
-  )
+    bearer_token: token["access_token"],
+  }
+end
+
+def build_apps_client(api_endpoint:, api_version:, ssl_options:, bearer_token:)
+  Kubeclient::Client.new("#{api_endpoint}/apis/apps", api_version, ssl_options: ssl_options, auth_options: { bearer_token: bearer_token })
+end
+
+def build_kube_client(api_endpoint:, api_version:, ssl_options:, bearer_token:)
+  Kubeclient::Client.new(api_endpoint, api_version, ssl_options: ssl_options, auth_options: { bearer_token: bearer_token })
 end
 
 FunctionsFramework.cloud_event "mc-scale" do |event|
-  apps_client = build_kube_client
+  kube_options = kube_client_options
+  apps_client = build_apps_client(**kube_options)
+  kube_client = build_kube_client(**kube_options)
   message = JSON.parse(Base64.decode64(event.data.dig("message", "data")))
 
   follow_up_options = {
@@ -69,14 +77,49 @@ FunctionsFramework.cloud_event "mc-scale" do |event|
         Scaling server down succeeded.
       MSG
     elsif subcommand == "status"
+      statefulset = apps_client.get_stateful_set("mc-lutova", "default")
+      sts_status = statefulset.status
+
+      if sts_status.replicas.zero?
+        send_follow_up(<<~MSG, **follow_up_options)
+          The server is off.
+          #{statefulset.metadata.name} → ready [✗]
+        MSG
+        return "Handled /#{event_name} #{subcommand}"
+      elsif sts_status.readyReplicas == sts_status.replicas
+        send_follow_up(<<~MSG, **follow_up_options)
+          The server is on.
+          #{statefulset.metadata.name} → ready [✓]
+        MSG
+        return "Handled /#{event_name} #{subcommand}"
+      end
+
+      pod = kube_client.get_pod("mc-lutova-0", "default")
+      containers = pod.status.containerStatuses.map do |container|
+        status = +"#{container.name} → started ["
+        status << (container.started ? "✓" : "✗")
+        status << "] ready ["
+        status << (container.ready ? "✓" : "✗")
+        status << "]"
+        status
+      end.join("\n    ")
+
       send_follow_up(<<~MSG, **follow_up_options)
-        The server is off?
+        #{statefulset.kind}(#{statefulset.metadata.name}) → ready [✗]
+          replicas=#{sts_status.replicas}
+          currentReplicas=#{sts_status.currentReplicas}
+          readyReplicas=#{sts_status.readyReplicas}
+
+        #{pod.kind}(#{pod.metadata.name}) → #{pod.status.phase.upcase}
+          containers=[
+            #{containers}
+          ]
       MSG
     else
       return "Unknown subcommand: #{subcommand}"
     end
 
-    "Handling /#{event_name} #{subcommand}"
+    "Handled /#{event_name} #{subcommand}"
   else
     "Unknown Event: /#{event_name}"
   end
